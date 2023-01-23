71804de6fbcb3249720684b7424d6dcab6dea81e
Rossamylne <117632558+Rossamylne@users.noreply.github.com>
2023-01-23 08:54:18 +0100

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,24 +1,22 @@
-def monnaie_a_rendre(argent):
-    '''
-    Fonction qui donne la monnaie à rendre en fonction d'un montant donné.
-    A l'aide d'une liste qui contient les différentes monnaie qu'on puisse rendre,
-    on parcours cette liste et on regarde si la valeur séléctionnée peut être déduite
-    de la somme donnée en argument. Si la valeur séléctionnée peut être déduite, on sort de la boucle 
-    et on recommence à parcourir la liste des différentes monnaie qu'on peut potentiellement rendre.
-    On effectue cela jusqu'à ce que la somme mise en argument sois égale à 0.
+Singe 1: Onk! (Il regarde autour de lui)
+
+Singe 2: Onk onk! (Il lui répond)
+
+Singe 1: Ouga! (Il montre quelque chose au loin)
+
+Singe 2: Onk onk! (Il regarde dans la direction indiquée)
+
+Singe 1: Onk onk ouga! (Il semble excité)
+
+Singe 2: Ouga ouga! (Il semble d'accord)
+
+Singe 1: Onk onk! (Il se lève et part dans la direction indiquée)
+
+Singe 2: Onk onk! (Il le suit)
+
+(Les singes quittent la scène)
+
+Note: Cette pièce de théâtre est très simpliste et n'a pas de signification précise, je vous invite à développer le scénario pour une pièce plus complète.
 
 
-    Entrée ------> une somme dont on veut connaître la somme a rendre
-
-    Sortie ------> la somme à rendre
-    '''
-    monnaies = [500, 200, 100, 50, 20, 10, 5, 2, 1]
-    while argent > 0:
-        for monnaie in monnaies:
-            if monnaie <= argent:
-                argent -= monnaie
-                print(f'Il faut rendre {monnaie} euros')
-                break
-
-monnaie_a_rendre(595)
 
